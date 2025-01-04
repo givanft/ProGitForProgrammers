0c5a65b7faf628aa0e310b3d53cbd5932a474660
Ivan Gorshenin <anfield_road@mail.ru>
2025-01-04 22:24:48 +0300

    Add writeline indicating we are in command line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
         }
     }
 }
