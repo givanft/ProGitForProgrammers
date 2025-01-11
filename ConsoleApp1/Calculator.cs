4ed96e5a0487db1fbcf97cca0025d79b40df5316
Ivan Gorshenin <anfield_road@mail.ru>
2025-01-11 22:08:40 +0300

    d

--- ConsoleApp1/Calculator.cs
+++ ConsoleApp1/Calculator.cs
@@ -19,11 +19,7 @@ namespace ProGitForProgrammers
         }
         public int Divide(int lhs, int rhs)
         {
-<<<<<<< HEAD
-            return rhs * 10;
-=======
             return lhs / rhs;
->>>>>>> master
         }
 
     }
