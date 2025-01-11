cbd089fa9a2a90097a460f73834a07f8ee7816a1
Ivan Gorshenin <anfield_road@mail.ru>
2025-01-11 21:36:07 +0300

    Modify the MultiplyBy10

--- ConsoleApp1/Calculator.cs
+++ ConsoleApp1/Calculator.cs
@@ -17,14 +17,6 @@ namespace ProGitForProgrammers
         {
             return lhs - rhs;
         }
-        public int Multiply(int lhs, int rhs)
-        {
-            return lhs * rhs;
-        }
-        public int MultiplyBy10(int lhs, int rhs)
-        {
-            return (lhs * rhs) * 10;
-        }
         public int Divide(int lhs, int rhs)
         {
             return lhs / rhs;
